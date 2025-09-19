e4a8693b69470dbe3023af99d84ee7e47fad7e9f
kama_ds10 <kamazd1234@gmail.com>
2025-09-19 21:45:32 +0100

    chore: restart Rock Paper Scissors project
    - Initialized fresh project setup
    - Added getComputerChoice() for random computer selection
    - Added getHumanChoice() for user input handling
    - Logged outputs to console for initial testing

--- app.js
+++ app.js
@@ -1,59 +1,28 @@
-// console.log("Hello World")
 
+function getComputerChoice(){
 
+    const choice = ['rock','paper','Scissors'];
+    const randomChioce = Math.floor(Math.random()* choice.length)
+    const decision = choice[randomChioce];
+    return decision
 
-function getComputerChoice() {
-  const choose = prompt("Enter rock, paper, or scissors:");
-  if (choose === "rock" || choose === "paper" || choose === "scissors") {
-    return choose;
-  } else {
-    console.log("Invalid choose! Please enter rock, paper, or scissors.");
-    return getComputerChoice(); 
-  }
-}
-// console.log(getComputerChoice("rock"))
-
-function getHumanChoice() {
-  const choice = prompt("Enter rock, paper, or scissors:");
-
-  if (choice === "rock" || choice === "paper" || choice === "scissors") {
-    return choice;
-  } else {
-    console.log("Invalid choice! Please type rock, paper, or scissors.");
-    return getHumanChoice(); 
-  }
 }
 
-// Test it
+getComputerChoice();
+console.log(getComputerChoice())
+
+
+function getHumanChoice(){
+    const userInput = prompt('Choose between Rock,Paper or Scissors');
+
+    if (userInput === 'rock'){
+        console.log('Rock');
+    }else if (userInput === 'paper'){
+        console.log('Paper');
+    }else{
+        console.log('Scissor')
+    }
+}
+
+getHumanChoice();
 console.log(getHumanChoice());
-
-
-let humanScore = 0;
-let computerScore = 0;
-
-
-function playRound(humanChoice,computerChoice){
-
-    // humanChoice = humanChoice.toLowerCase();
-
-    if (humanChoice === computerChoice) {
-    console.log("It's a tie! You both chose " + humanChoice);
-  } else if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")
-  ) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    humanScore++;
-  } else {
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    computerScore++;
-  }
- console.log(`Score → Human: ${humanScore}, Computer: ${computerScore}`);
-
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
