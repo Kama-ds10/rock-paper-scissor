a65d4ee77f46af631c3fea776daa271fa6be0b12
kama_ds10 <kamazd1234@gmail.com>
2025-09-19 23:33:05 +0100

    feat: update playRound to track scores
    - Increment humanScore or computerScore using ++ based on round winner
    - Added score display after each round
    - Ensures game state updates correctly with every play

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 function getComputerChoice(){
 
-    const choice = ['rock','paper','Scissors'];
+    const choice = ['Rock','Paper','Scissors'];
     const randomChioce = Math.floor(Math.random()* choice.length)
     const decision = choice[randomChioce];
     return decision
@@ -15,9 +15,9 @@ console.log(getComputerChoice())
 function getHumanChoice(){
     const userInput = prompt('Choose between Rock,Paper or Scissors');
 
-    if (userInput === 'rock'){
+    if (userInput === 'Rock'){
         console.log('Rock');
-    }else if (userInput === 'paper'){
+    }else if (userInput === 'Paper'){
         console.log('Paper');
     }else{
         console.log('Scissor')
@@ -25,8 +25,43 @@ function getHumanChoice(){
 }
 
 getHumanChoice();
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
 
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice,computerChoice){
+
+    const inputHuman = humanChoice.toLowerCase();
+    const inputComputer = computerChoice.toLowerCase();
+    
+
+    if(inputHuman === inputComputer){
+        console.log(`It's a tie! Both chose ${inputHuman}`)
+        
+    }
+
+    else if(
+    (inputHuman === 'rock' && inputComputer === 'scissors') ||
+    (inputHuman === 'paper' && inputComputer === 'rock') ||
+    (inputHuman === 'scissors' && inputComputer === 'paper')){
+
+        console.log(`You win! ${inputHuman} beats ${inputComputer}
+        
+`)
+humanScore++;
+    }else{
+
+     console.log(`You lose! ${inputComputer} beats ${inputHuman}.`);
+     computerScore++ ;
+
+    }
+
+        console.log(`Score → Human: ${humanScore}, Computer: ${computerScore}`);
+
+// playRound();
+
+}
+
+(playRound('Rock','Paper'));
